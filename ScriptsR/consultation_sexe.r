19d85e58c21983f8d130afcfda222a171bf4145b
Rom168 <romain.huntzinger@viacesi.fr>
2023-04-19 16:47:22 +0200

    consultation par sexe

--- ScriptsR/consultation_sexe.r
+++ ScriptsR/consultation_sexe.r
@@ -1,9 +1,9 @@
 library(ggplot2)
 library(ggthemes)
 library(dplyr)
-library(ggiraph)
 
-data <- read.csv("C:\\Users\\romai\\WebstormProjects\\ProjetCHU\\CSVrequetes\\TotalConsultAge.csv", sep=";")
+
+data <- read.csv("C:\\Users\\romai\\WebstormProjects\\ProjetCHU\\CSVrequetes\\TotalConsultSexe.csv", sep=";")
 
 data$AgeGroup <- cut(data$Age, seq(0, 111, by=10), right = FALSE)
 
@@ -13,13 +13,13 @@ data %>%
   summarize(PercentHosp = sum(Total)/sum(data$Total) * 100) %>%
     ggplot(aes(x=AgeGroup, y=PercentHosp, tooltip = paste0(sprintf("%.2f", PercentHosp), "%")))+
     geom_bar(stat= "identity", fill="steelblue") +
-    labs(title = "Taux d'hospitalisation par tranche d'âge",
-       x = "Tranche d'Age",
+    labs(title = "Taux d'hospitalisation par sexe",
+       x = "Sexe",
        y = "Pourcentage") +
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 
-ggsave("A:/Data/Total_Hospi_Age.png", width=10, height=5)
+ggsave("A:/Data/Total_Hospi_Sexe.png", width=10, height=5)
 
 
 
